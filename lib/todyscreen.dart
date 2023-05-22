0e9898abf166ec03a3847d40ab6f0351e4be047e
randeeshapalliyaguruge <cb009991@students.apiit.lk>
2023-05-22 16:41:47 +0530

    Before Screen slide

--- lib/todyscreen.dart
+++ lib/todyscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class TodayScreen extends StatefulWidget {
   const TodayScreen({Key? key}) : super(key: key);
 
@@ -8,12 +9,159 @@ class TodayScreen extends StatefulWidget {
 }
 
 class _TodayScreenState extends State<TodayScreen> {
+  double screenHeight =  0;
+  double screenWidth =  0;
+
+  Color primary = const Color(0xffeef444c);
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
     return  Scaffold(
-      body: Center(
-        child: Text("Today"),
-      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: Text(
+                "Welcome",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontFamily: "NexaRegular",
+                  fontSize: screenWidth / 20,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Employee",
+                style: TextStyle(
+                  fontFamily: "NexaBold",
+                  fontSize: screenWidth / 18,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: Text(
+                "Today's Status",
+                style: TextStyle(
+                  fontFamily: "NexaBold",
+                  fontSize: screenWidth / 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 12 , bottom: 32),
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(2 ,2),
+                  )
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                              "Check In",
+                            style: TextStyle(
+                              fontFamily: "NexaRegular",
+                              fontSize: screenWidth / 20,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Text(
+                            "09.30",
+                            style: TextStyle(
+                              fontFamily: "NexaBold",
+                              fontSize: screenWidth / 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Check Out",
+                          style: TextStyle(
+                            fontFamily: "NexaRegular",
+                            fontSize: screenWidth / 20,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Text(
+                          "--/--",
+                          style: TextStyle(
+                            fontFamily: "NexaBold",
+                            fontSize: screenWidth / 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: RichText(
+                text: TextSpan(
+                  text: "11",
+                  style: TextStyle(
+                    color: primary,
+                    fontSize: screenWidth / 18,
+                    fontFamily: "NexaBold",
+                  ),
+                  children: [
+                    TextSpan(
+                      text: " Jan 2020",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth / 20,
+                        fontFamily: "NexaBold",
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "12:00:01 PM",
+                style: TextStyle(
+                  fontFamily: "NexaRegular",
+                  fontSize: screenWidth / 20,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      )
     );
   }
 }
