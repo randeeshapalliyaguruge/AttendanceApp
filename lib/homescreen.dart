c5a0157f41de14e4d68fc56349e508fe23d85c54
randeeshapalliyaguruge <cb009991@students.apiit.lk>
2023-05-22 10:24:16 +0530

    Creating Bottom Navigation Bar
    Creating the pages for Calender,today, profile screens

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,4 +1,8 @@
+import 'package:attendance_app/calenderscreen.dart';
+import 'package:attendance_app/profilescreen.dart';
+import 'package:attendance_app/todyscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,11 +12,97 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double screenHeight =  0;
+  double screenWidth =  0;
+
+  Color primary = const Color(0xffeef444c);
+
+  int currentIndex = 0;
+
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.calendarAlt,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.user,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HOME"),
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: const [
+          CalenderScreen(),
+          TodayScreen(),
+          ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: const EdgeInsets.only(
+          left: 12,
+          right: 12,
+          bottom: 24,
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
+        child:  ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for(int i =0; i < navigationIcons.length; i++)...<Expanded>{
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentIndex = i;
+                      });
+                    },
+                    child: Container(
+                      height: screenHeight,
+                      width: screenWidth,
+                      color: Colors.white,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                                navigationIcons[i],
+                                color: i == currentIndex ?  primary : Colors.black54,
+                                size: i == currentIndex ? 30 : 26,
+                            ),
+                            i == currentIndex ? Container(
+                              margin: EdgeInsets.only(top: 6),
+                              height: 3,
+                              width: 22,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(Radius.circular(40)),
+                                color: primary,
+                              ),
+                            ) : const SizedBox(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              }
+            ],
+          ),
+        ),
       ),
     );
   }
